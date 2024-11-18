77a7d1ef5fdc361747c30fcde07b6326d7a2a33f
Erollercoaster <erollfern@gmail.com>
2024-11-18 12:45:51 +0800

    Completed trade functionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,6 +6,7 @@ class StocksController < ApplicationController
       @stock = Stock.find_by(symbol: params[:symbol].upcase)
       flash.now[:alert] = "Stock symbol not found. Please try another." if @stock.nil?
     end
+    @stocks = Stock.all
   end
 
   def show
@@ -20,14 +21,17 @@ class StocksController < ApplicationController
           flash.now[:alert] = result[:error]
         end
       end
+      
+      @user_stock_quantity = current_user.transactions.where(stock: @stock, sold: false).sum(:quantity)
+      @recent_transactions = @stock.transactions.order(created_at: :desc).limit(5)
+      @transaction = Transaction.new
     end
   end
 
   private
 
   def set_stock
-    @stock = Stock.find_by(symbol: params[:symbol].upcase)
-    redirect_to stocks_path, alert: "Stock symbol not found" if @stock.nil?
+    @stock = Stock.find_by(symbol: params[:symbol]) || Stock.find_by(id: params[:symbol])
+    redirect_to stocks_path, alert: "Stock not found." unless @stock
   end
-
 end
