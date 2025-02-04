363b6f44749a07c5978cd82d174cbfb6b0213572
Erollercoaster <erollfern@gmail.com>
2025-02-04 14:18:56 +0800

    Fixed migrations

--- db/migrate/20241118224628_reverse_add_confirmable_to_users.rb
+++
@@ -1,7 +0,0 @@
-class ReverseAddConfirmableToUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :confirmed_at
-    remove_column :users, :confirmation_sent_at
-    remove_column :users, :unconfirmed_email
-  end
-end
